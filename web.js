f8b2cc9d57dc08bc1aad0c3b87392120cb0624d9
Marco Fischer <marcofischer.ch@gmail.com>
2013-07-06 12:12:35 +0200

    web.js

--- web.js
+++ web.js
@@ -2,14 +2,13 @@ var express = require('express');
 
 var fs = require('fs');
 
-var buffer = new Buffer();
-	
 console.log("Data: " +buffer);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString("utf-8",fs.readFileSync('index.html')));
+	var buffer = new Buffer("utf-8",fs.readFileSync('index.html'));
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
